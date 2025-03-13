711735e549aa231caccf44e8c60abb5b9c1e59e8
Deepa <deepavaishali@gmail.com>
2025-03-13 18:18:15 +0530

    march13newfiles

---
+++ src/ex_01_Java_Basic/this_is_$_symbol.java
@@ -0,0 +1,4 @@
+package ex_01_Java_Basic;
+
+public class this_is_$_symbol {
+}
---
+++ src/ex_01_Java_Basic/this_is_a_long_name_for_ideentifier.java
@@ -0,0 +1,5 @@
+package ex_01_Java_Basic;
+
+public class this_is_a_long_name_for_ideentifier {
+    //this also acceptable
+}
