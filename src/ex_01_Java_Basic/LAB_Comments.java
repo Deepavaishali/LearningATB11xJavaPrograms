a68bbe85a1cb07ee37a39be23884fc9f1571f69e
Deepa <deepavaishali@gmail.com>
2025-03-13 17:41:35 +0530

    march 13 comments

---
+++ src/ex_01_Java_Basic/LAB_Comments.java
@@ -0,0 +1,20 @@
+package ex_01_Java_Basic;
+
+public class LAB_Comments {
+    public static void main(String[] args) {
+        //Author - deepa
+        /*this comment is used anywhere between codes.
+        this is for explanation and documnetatpurpose
+        multiple times we can use it .
+         represesnts multi comments
+        these comments wont be executed by compiler
+         */
+        System.out.println("welcome to comment section");
+        System.out.println("welcome to comment section");
+        System.out.println("welcome to comment section");
+        //this continuous statements is due to the short cut ctrl+D
+        //if you want to delete the statmenet, use shortcut ctrl+Y
+
+    }
+
+}
