02018c57a42921ca26badb410b89a0fc473dc71e
Deepa <deepavaishali@gmail.com>
2025-03-14 11:44:59 +0530

    march 14 file

---
+++ src/ex_01_Java_Basic/LAB011_mainfunction_multi.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basic;
+
+public class LAB011_mainfunction_multi {
+    public static void main(String[] args) {
+
+    }
+    //can have multiple main function.but jvm recognise this and run the program
+
+    public static void main(String args) {
+
+    }
+
+    public static void main(int args) {
+
+    }
+}
